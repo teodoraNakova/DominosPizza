4b65467b660619a717b4767ad0fa18c3a253bcca
Martin <mbinev2@gmail.com>
2017-04-13 00:26:13 +0300

    working homepage

--- src/controller/categoryServlet.java
+++ src/controller/categoryServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Product;
 import model_dao.ProductDAO;
 
 /**
@@ -21,15 +22,20 @@ public class categoryServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("ehooooooooooooooooooo");
 		List<String> categories = null;
+		List<Product> products = new ArrayList<>();
 		try {
 			categories = new ArrayList<String>(ProductDAO.getInstance().getAllProducts().keySet());
+			for (String category : categories) {
+				products.addAll(ProductDAO.getInstance().getAllProducts().get(category));
+			}
 		} catch (SQLException e) {
 			System.out.println("MenuServlet problem");
 			e.printStackTrace();
 		}
+		request.getSession().setAttribute("products", products);
 		request.getSession().setAttribute("categories", categories);
 		System.out.println(categories);
-		request.getRequestDispatcher("menu.jsp").forward(request, response);
+		request.getRequestDispatcher("products.jsp").forward(request, response);
 	}
 
 
